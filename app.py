f508ce67ba4e9931a4eaa1478498e72dfbe0e4ee
Shaik Azhar <sahmed@ooyala.com>
2018-08-30 03:09:37 +0530

    Corrected the libraries and flow of the code

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+# importing all the modules
 try:
     import sys
 except ImportError:
      print ('sys : Module not found.  ')
+
 try:
     import os
 except ImportError:
      print ('os : Module not found')
+
 try:
     from flask import *
 except ImportError:
@@ -14,24 +17,25 @@ except ImportError:
 try:
     import json
 except ImportError:
-  print'  json : library not found.  '
-  print ('flask : Module not found')
+  print 'json : library not found'
 
 try:
     import requests
 except ImportError:
-  print'  requests : library not found.  '
+  print 'requests : library not found'
 
 try:
     import urllib
 except ImportError:
-  print'  urllib : library not found.  '
+  print'urllib : library not found'
 
 sys.path.append('./helpers')
+
 
 from utils import *
 from Validation import *
 from FormsCheck import *
+
 
 app = Flask(__name__)
 app.secret_key = 'Let it be a secrete'
@@ -48,7 +52,14 @@ def index():
 @app.route("/<string:search>",methods=['GET', 'POST'])
 def searchEmbed(search):
     form = EmbedSearch(request.form)
-    return render_template('index.html',form = form)
+    inputFromPage = str(request.form['lineItemId']).replace(" ","")
+    lineItemdata = getLineItemResponse( inputFromPage)
+    print lineItemdata
+    return render_template('details.html',form = form, data=None)
+
+
+
 
 if __name__ == '__main__':
+
     app.run(debug=True)
