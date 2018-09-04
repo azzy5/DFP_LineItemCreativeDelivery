1f1925eace23960e6b81ff6fa9aa4bee81f9bf6a
Shaik Azhar <sahmed@ooyala.com>
2018-09-04 09:37:33 +0530

    Improved the code for filtering and handling the responses.

--- helpers/Validation.py
+++ helpers/Validation.py
@@ -3,9 +3,8 @@ from random import randint
 from googleads import ad_manager
 
 version = 'v201802'
-def getLineItemResponse(lineitemID):
-    ad_manager_client = ad_manager.AdManagerClient.LoadFromStorage()
-    lineItem_service = ad_manager_client.GetService('LineItemService', version = version)
+def getLineItemResponse(client, lineitemID):
+    lineItem_service = client.GetService('LineItemService', version = version)
     lineitem_statement = (ad_manager.StatementBuilder()
             .Where('lineItemId = :lineItemId')
             .Limit(1)
@@ -19,26 +18,26 @@ def getLineItemResponse(lineitemID):
 
 
 def getCreativeResponse(client, creativeID):
+
     creative_service = client.GetService('CreativeService', version = version)
     creative_statement = (ad_manager.StatementBuilder()
-               .Where('creativeType = :id')
-               .WithBindVariable('id', creativeID))
-    creative_response = creative_service.getLineItemsByStatement(creative_statement.ToStatement())
+               .Where('id = :creativeid')
+               .WithBindVariable('creativeid', creativeID))
+    creative_response = creative_service.getCreativesByStatement(creative_statement.ToStatement())
+    #print "Creative details : " + str(creative_response)
     if 'results' in creative_response and len(creative_response['results']):
-        print "Creative details : " + str(creative_response)
-        return creative_response
+        return creative_response['results'][0]
     else :
-        print "something went wrong"
         return False
 
-def getLICAresponse(lineitemID):
+def getLICAresponse(client,lineitemID):
+    # ad_manager_client = ad_manager.AdManagerClient.LoadFromStorage()
     lica_service = client.GetService('LineItemCreativeAssociationService', version = version)
     lica_statement = (ad_manager.StatementBuilder()
                .Where('lineItemId = :lineItemId')
                .WithBindVariable('lineItemId', lineitemID))
-    lica_response = lica_service.getLineItemsByStatement(lica_statement.ToStatement())
+    lica_response = lica_service.getLineItemCreativeAssociationsByStatement(lica_statement.ToStatement())
     if 'results' in lica_response and len(lica_response['results']):
-        print "  LICA details Details : " + str(lica_response)
         return lica_response
     else :
         print "something went wrong"
